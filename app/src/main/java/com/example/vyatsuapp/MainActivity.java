55f0b94238a9af683c6f0d171a1fc8bf4c03cb3f
11qfour <futurenature31@gmail.com>
2024-01-25 12:58:39 +0300

    ch

--- app/src/main/java/com/example/vyatsuapp/MainActivity.java
+++ app/src/main/java/com/example/vyatsuapp/MainActivity.java
@@ -24,7 +24,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     private TextView result;
     private static final String VyatsuTimeTableURL = "https://www.vyatsu.ru/studentu-1/spravochnaya-informatsiya/raspisanie-zanyatiy-dlya-studentov.html";
-    private static final String fullTimeTimeTable = "";
+    private static final String fullTimeTimeTable = "123456";
     String[] faculty = {"ПЕД", "ИБиБ", "ХиЭ", "ФАВТ", "ФИПНиК", "ФКиФМН"};
 
     private class NetworkTask extends AsyncTask<Void, Void, Document> {
