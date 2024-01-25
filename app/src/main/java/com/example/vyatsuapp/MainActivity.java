aed338f8660106f5ec31bae772a881b72738b680
unclled <dande.chuprakov04@gmail.com>
2024-01-25 16:25:28 +0300

    connect_Vyatsu

--- app/src/main/java/com/example/vyatsuapp/MainActivity.java
+++ app/src/main/java/com/example/vyatsuapp/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.vyatsuapp;
 
-import static com.example.vyatsuapp.utils.NetworkUtils.generateURL;
-import static com.example.vyatsuapp.utils.ParseToGetFaculties.getFaculties;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,43 +9,35 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.example.vyatsuapp.R;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String VyatsuURL = "https://www.vyatsu.ru/";
+    private final String FullTimeTimetable = "studentu-1/spravochnaya-informatsiya/raspisanie-zanyatiy-dlya-studentov.html";
+    // расписание для очного обучения
+    private final String DistanceCertification = "internet-gazeta/raspisanie-promezhutochnoy-attestatsii-obuchayusch-1.html";
+    // расписание промежуточной аттестации и занятий для заочников
+    private final String FullTimeAndDistance = "studentu-1/spravochnaya-informatsiya/raspisanie-zanyatiy-studentov-ochno-zaochnoy-formy.html";
+    // расписание промежуточной аттестации и занятий для очно-заочного обучения
+    private final String FullTimeCertification = "internet-gazeta/raspisanie-sessiy-obuchayuschihsya-na-2016-2017-uc.html";
+    // расписание промежуточной аттестации для очников
+    private final String PracticeCertification = "internet-gazeta/raspisanie-promezhutochnoy-attestatsii-obuchayusch-1.html";
+    // расписание промежуточной аттестации для обучающихся по практике
+
     private TextView result;
-    private static final String VyatsuTimeTableURL = "https://www.vyatsu.ru/studentu-1/spravochnaya-informatsiya/raspisanie-zanyatiy-dlya-studentov.html";
-    private static final String fullTimeTimeTable = "123456";
-    String[] faculty = {"ПЕД", "ИБиБ", "ХиЭ", "ФАВТ", "ФИПНиК", "ФКиФМН"};
 
-    private class NetworkTask extends AsyncTask<Void, Void, Document> {
-        @Override
-        protected Document doInBackground(Void... params) {
-            try {
-                return Jsoup.connect(VyatsuTimeTableURL).maxBodySize(0).get();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Document document) {
-            if (document != null) {
-                var faculty = document.select("h4");
-                String facultyText = faculty.text();
-
-                result.setText(facultyText);
-            } else {
-
-            }
-        }
-    }
+    private static final String[] typeOfEducation = {"Очно", "Очно-заочно", "Заочно"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,21 +49,65 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.timetable);
 
         ArrayAdapter<String> facultyAdapter = new ArrayAdapter<>(this,
-                                            android.R.layout.simple_spinner_item, faculty);
+                android.R.layout.simple_spinner_item, typeOfEducation);
         facultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner spFaculty = (Spinner) findViewById(R.id.chooseFaculty);
+        Spinner spFaculty = findViewById(R.id.chooseFaculty);
         spFaculty.setAdapter(facultyAdapter);
-
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new NetworkTask().execute();
+                String selectedItem = spFaculty.getSelectedItem().toString();
+                String selectedEducationForm;
+                if (selectedItem.equals("Очно")) {
+                    selectedEducationForm = FullTimeTimetable;
+                } else if (selectedItem.equals("Очно-заочно")) {
+                    selectedEducationForm = FullTimeAndDistance;
+                } else {
+                    selectedEducationForm = DistanceCertification;
+                }
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            URL url = new URL(VyatsuURL + selectedEducationForm);
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("GET");
+
+                            int responseCode = connection.getResponseCode();
+                            if (responseCode == HttpURLConnection.HTTP_OK) {
+                                InputStream inputStream = connection.getInputStream();
+                                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                                StringBuilder response = new StringBuilder();
+                                String line;
+                                while ((line = bufferedReader.readLine()) != null) {
+                                    response.append(line);
+                                }
+                                bufferedReader.close();
+                                inputStream.close();
+
+                                final String htmlResponse = response.toString();
+                                Document document = Jsoup.parse(htmlResponse);
+                                final String facultyText = document.select("div.fak_name").text();
+
+                                runOnUiThread(() -> {
+                                    result.setText(facultyText);
+                                });
+                            }
+
+                            connection.disconnect();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
             }
         };
 
         confirmCourseFacultyButton.setOnClickListener(onClickListener);
-
     }
 }
---
+++ app/src/main/java/com/example/vyatsuapp/utils/HTTPHandler.java
@@ -0,0 +1,41 @@
+package com.example.vyatsuapp.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPHandler {
+    public String makeHttpRequest(String url) {
+        String response = null;
+        try {
+            URL requestUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                bufferedReader.close();
+                inputStream.close();
+
+                response = stringBuilder.toString();
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+}
+
